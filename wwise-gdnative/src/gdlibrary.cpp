426cf1a4204acc8aa2ba2bfce8f3c07ab10b02fe
Jorge Garcia <jorge.gameaudio@gmail.com>
2020-05-09 19:26:44 +0200

    Format tweaks

--- wwise-gdnative/src/gdlibrary.cpp
+++ wwise-gdnative/src/gdlibrary.cpp
@@ -2,18 +2,20 @@
 
 const godot_gdnative_core_api_struct* godot_api = nullptr;
 
-extern "C" void GDN_EXPORT wwise_gdnative_init(godot_gdnative_init_options * o) {
+extern "C" void GDN_EXPORT wwise_gdnative_init(godot_gdnative_init_options * o) 
+{
 	godot_api = o->api_struct;
-
 	godot::Godot::gdnative_init(o);
 }
 
-extern "C" void GDN_EXPORT wwise_gdnative_terminate(godot_gdnative_terminate_options * o) {
+extern "C" void GDN_EXPORT wwise_gdnative_terminate(godot_gdnative_terminate_options * o) 
+{
 	godot_api = nullptr;
 	godot::Godot::gdnative_terminate(o);
 }
 
-extern "C" void GDN_EXPORT wwise_nativescript_init(void* handle) {
+extern "C" void GDN_EXPORT wwise_nativescript_init(void* handle) 
+{
 	godot::Godot::nativescript_init(handle);
 	godot::register_class<godot::Wwise>();
 }
