c244a9ad3da087415253989a491a9fb348a8e951
Alessandro <alessandro.fama@ymail.com>
2020-05-10 13:36:28 +0200

    Body Enter/Body Exit signals

--- wwise-gdnative/gdnative-demo/wwise/nodes/ak_event.gd
+++ wwise-gdnative/gdnative-demo/wwise/nodes/ak_event.gd
@@ -22,11 +22,17 @@ func _exit_tree():
 func _ready():
 	handle_game_event(AkUtils.GameEvent.READY);
 	
-func _on_Area_area_entered(_area:Object):
+func _on_area_entered(_area:Object):
 	handle_game_event(AkUtils.GameEvent.AREA_ENTER);
 	
-func _on_Area_area_exited(_area:Object):
+func _on_area_exited(_area:Object):
 	handle_game_event(AkUtils.GameEvent.AREA_EXIT);
+	
+func _on_body_entered(_body:Object):
+	handle_game_event(AkUtils.GameEvent.BODY_ENTER);
+	
+func _on_body_exited(_body:Object):
+	handle_game_event(AkUtils.GameEvent.BODY_EXIT);
 	
 func handle_game_event(gameEvent:int):
 	if trigger_on == gameEvent:
