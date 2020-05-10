452fd9bf958836c28c32f0ef407e5520dda0ab4f
Alessandro <alessandro.fama@ymail.com>
2020-05-10 13:39:59 +0200

    Added bank signals

--- wwise-gdnative/gdnative-demo/wwise/nodes/ak_bank.gd
+++ wwise-gdnative/gdnative-demo/wwise/nodes/ak_bank.gd
@@ -17,6 +17,18 @@ func _exit_tree():
 func _ready():
 	handle_game_event(AkUtils.GameEvent.READY);
 	
+func _on_area_entered(_area:Object):
+	handle_game_event(AkUtils.GameEvent.AREA_ENTER);
+	
+func _on_area_exited(_area:Object):
+	handle_game_event(AkUtils.GameEvent.AREA_EXIT);
+	
+func _on_body_entered(_body:Object):
+	handle_game_event(AkUtils.GameEvent.BODY_ENTER);
+	
+func _on_body_exited(_body:Object):
+	handle_game_event(AkUtils.GameEvent.BODY_EXIT);
+	
 func handle_game_event(gameEvent:int):
 	if load_on == gameEvent:
 		load_bank()
