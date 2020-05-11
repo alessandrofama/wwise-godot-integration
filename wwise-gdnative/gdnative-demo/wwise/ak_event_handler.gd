53cfeb8e600d46d49160c332773eb709cadf691d
Alessandro <alessandro.fama@ymail.com>
2020-05-11 11:55:19 +0200

    Added support for groups/tags in area/body trigger methods in custom nodes and ak_event_handler

--- wwise-gdnative/gdnative-demo/wwise/ak_event_handler.gd
+++ wwise-gdnative/gdnative-demo/wwise/ak_event_handler.gd
@@ -1,6 +1,8 @@
 extends AkGameObj
 class_name AkEventHandler
 
+export(String) var group_tag
+
 func _enter_tree():
 	handle_game_event(AkUtils.GameEvent.TREE_ENTER);
 	
@@ -10,17 +12,21 @@ func _exit_tree():
 func _ready():
 	handle_game_event(AkUtils.GameEvent.READY);
 	
-func _on_area_entered(_area:Object):
-	handle_game_event(AkUtils.GameEvent.AREA_ENTER);
+func _on_area_entered(area:Object):
+	if group_tag.empty() or area.is_in_group(group_tag):
+		handle_game_event(AkUtils.GameEvent.AREA_ENTER);
 	
-func _on_area_exited(_area:Object):
-	handle_game_event(AkUtils.GameEvent.AREA_EXIT);
+func _on_area_exited(area:Object):
+	if group_tag.empty() or area.is_in_group(group_tag):
+		handle_game_event(AkUtils.GameEvent.AREA_EXIT);
 	
-func _on_body_entered(_body:Object):
-	handle_game_event(AkUtils.GameEvent.BODY_ENTER);
+func _on_body_entered(body:Object):
+	if group_tag.empty() or body.is_in_group(group_tag):
+		handle_game_event(AkUtils.GameEvent.BODY_ENTER);
 	
-func _on_body_exited(_body:Object):
-	handle_game_event(AkUtils.GameEvent.BODY_EXIT);
+func _on_body_exited(body:Object):
+	if group_tag.empty() or body.is_in_group(group_tag):
+		handle_game_event(AkUtils.GameEvent.BODY_EXIT);
 	
 func handle_game_event(_gameEvent:int):
 	pass
