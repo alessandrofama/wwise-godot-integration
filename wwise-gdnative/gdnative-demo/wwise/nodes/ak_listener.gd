6282a84cdb0e1658d5b5cc305694ad09eb9be756
Alessandro <alessandro.fama@ymail.com>
2020-06-01 12:14:14 +0200

    Added spatial listener setting in custom AkListener node

--- wwise-gdnative/gdnative-demo/wwise/nodes/ak_listener.gd
+++ wwise-gdnative/gdnative-demo/wwise/nodes/ak_listener.gd
@@ -1,9 +1,15 @@
 extends Spatial
 class_name AkListener
 
+export(bool) var is_spatial = false
+var last_position:Vector3
+
 func _ready() -> void:
 	Wwise.register_listener(self)
+	if is_spatial:
+		Wwise.register_spatial_listener(self)
 	Wwise.set_3d_position(self, get_global_transform())
 
 func _process(_delta) -> void:
-	Wwise.set_3d_position(self, get_global_transform())
+	if not get_global_transform().origin == last_position:
+		Wwise.set_3d_position(self, self.get_global_transform())
