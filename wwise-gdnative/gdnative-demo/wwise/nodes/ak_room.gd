e301411472fdbf0af45514737568b34cea4e83fa
Alessandro <alessandro.fama@ymail.com>
2020-06-07 00:12:57 +0200

    AkRoom polishing

--- wwise-gdnative/gdnative-demo/wwise/nodes/ak_room.gd
+++ wwise-gdnative/gdnative-demo/wwise/nodes/ak_room.gd
@@ -10,14 +10,19 @@ func _enter_tree():
 	connect("body_entered", self, "_on_body_enter")
 	#warning-ignore:return_value_discarded
 	connect("body_exited", self, "_on_body_exit")
-	
+
+# If any Event or Listener enters or exites this Area, it will added or removed
+# to/from this Room.	
 func _on_body_enter(body):
-	var children = body.get_children()
-	for child in children:
-		var subchildren = child.get_children()
-		for i in subchildren:
-			children.append(i)
-	for child in children:
+	var child_nodes = body.get_children()
+	# Getting all the childs nodes of the nodes above and appending them
+	# to the child_nodes array, since the Event or Listener could be nested 
+	# somewhere in the tree...
+	for child in child_nodes:
+		var sub_children = child.get_children()
+		for i in sub_children:
+			child_nodes.append(i)
+	for child in child_nodes:
 		if child is AkEvent:
 			if child.is_spatial:
 				add_game_obj_to_room(child, self)
@@ -25,12 +30,12 @@ func _on_body_enter(body):
 			add_game_obj_to_room(child, self)
 
 func _on_body_exit(body):
-	var children = body.get_children()
-	for child in children:
-		var subchildren = child.get_children()
-		for i in subchildren:
-			children.append(i)
-	for child in children:
+	var child_nodes = body.get_children()
+	for child in child_nodes:
+		var sub_children = child.get_children()
+		for i in sub_children:
+			child_nodes.append(i)
+	for child in child_nodes:
 		if child is AkEvent:
 			if child.is_spatial:
 				remove_game_obj_from_room(child)
