3a4e2a12e8366092475dbc17b4017f7e2f3f5bc5
Alessandro <alessandro.fama@ymail.com>
2020-06-23 15:51:03 +0200

    Simple Waapi connection test

--- waapiclient-gdnative/gdnative-demo/testing/connect.gd
+++ waapiclient-gdnative/gdnative-demo/testing/connect.gd
@@ -1,9 +1,11 @@
 extends Node
 
-func _ready():
-	var result = Wwise.connect("127.0.0.1", 8080)
-	print("Connect Result: ", result)
-	
-	var dict= Wwise.call("ak.wwise.core.remote.getConnectionStatus", "{}", "{}")
-	print(dict)
+func _input(_ev):
+	if Input.is_key_pressed(KEY_1):
+		var result = Wwise.connect("127.0.0.1", 8080)
+		print("Connect Result: ", result)
+		if result:
+			var dict = Wwise.call("ak.wwise.core.getInfo", "{}", "{}")
+			print(dict)
+
 
