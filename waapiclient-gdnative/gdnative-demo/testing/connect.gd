f3a94fe15c09ccc3cf411495633bee22cc0c7dd2
Jorge Garcia <jorge.gameaudio@gmail.com>
2020-06-26 12:12:26 +0200

    Refactored Waapi example and connection test

--- waapiclient-gdnative/gdnative-demo/testing/connect.gd
+++ waapiclient-gdnative/gdnative-demo/testing/connect.gd
@@ -2,10 +2,18 @@ extends Node
 
 func _input(_ev):
 	if Input.is_key_pressed(KEY_1):
-		var result = Wwise.connect("127.0.0.1", 8080)
-		print("Connect Result: ", result)
-		if result:
-			var dict = Wwise.call("ak.wwise.core.getInfo", "{}", "{}")
-			print(dict)
+		var connectResult = Waapi.connect("127.0.0.1", 8080)
+		print("Connect Result: ", connectResult)
+		
+		if connectResult:
+			var dict = Waapi.call("ak.wwise.core.getInfo", "{}", "{}")
+			var jsonDocument = JSON.parse(dict["resultString"])
+			
+			if jsonDocument.error == OK:
+				print(jsonDocument.result["branch"])
+				print(jsonDocument.result["copyright"])
+		else:
+			print("Wwise authoring isn't launched!")
 
-
+		if Waapi.is_connected():
+			Waapi.disconnect()
