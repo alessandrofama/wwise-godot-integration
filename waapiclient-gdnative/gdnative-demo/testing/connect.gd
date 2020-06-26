1a4569c84795beab6de03d4b200982cdcc0a7cbc
Jorge Garcia <jorge.gameaudio@gmail.com>
2020-06-26 12:21:30 +0200

    Added gut testing framework. Refactored connection test script

--- waapiclient-gdnative/gdnative-demo/testing/connect.gd
+++ waapiclient-gdnative/gdnative-demo/testing/connect.gd
@@ -1,19 +1,18 @@
 extends Node
 
-func _input(_ev):
-	if Input.is_key_pressed(KEY_1):
-		var connectResult = Waapi.connect("127.0.0.1", 8080)
-		print("Connect Result: ", connectResult)
+func _init():
+	var connectResult = Waapi.connect("127.0.0.1", 8080)
+	print("Connect Result: ", connectResult)
+	
+	if connectResult:
+		var dict = Waapi.call("ak.wwise.core.getInfo", "{}", "{}")
+		var jsonDocument = JSON.parse(dict["resultString"])
 		
-		if connectResult:
-			var dict = Waapi.call("ak.wwise.core.getInfo", "{}", "{}")
-			var jsonDocument = JSON.parse(dict["resultString"])
-			
-			if jsonDocument.error == OK:
-				print(jsonDocument.result["branch"])
-				print(jsonDocument.result["copyright"])
-		else:
-			print("Wwise authoring isn't launched!")
+		if jsonDocument.error == OK:
+			print(jsonDocument.result["branch"])
+			print(jsonDocument.result["copyright"])
+	else:
+		print("Wwise authoring isn't launched!")
 
-		if Waapi.is_connected():
-			Waapi.disconnect()
+	if Waapi.is_connected():
+		Waapi.disconnect()
