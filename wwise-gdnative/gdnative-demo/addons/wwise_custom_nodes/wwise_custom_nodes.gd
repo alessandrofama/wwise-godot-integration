bade0ae8ccdcef59ed85aae19bfb95929a9a821e
Jorge Garcia <info@jorgegarciamartin.com>
2020-07-09 19:10:05 +0200

    Renamed Wwise addon as Wwise Custom Nodes. Updated Godot project

--- wwise-gdnative/gdnative-demo/addons/wwise_custom_nodes/wwise_custom_nodes.gd
+++ wwise-gdnative/gdnative-demo/addons/wwise_custom_nodes/wwise_custom_nodes.gd
@@ -7,26 +7,26 @@ var ak_event_gizmo = AkEventGizmo.new()
 
 func _enter_tree():
 	# 3D Nodes
-	add_custom_type("AkEvent", "Spatial", preload("res://wwise/nodes/ak_event.gd"), preload("res://addons/wwise/wwise_node.png"))
-	add_custom_type("AkBank", "Spatial", preload("res://wwise/nodes/ak_bank.gd"), preload("res://addons/wwise/wwise_node.png"))
-	add_custom_type("AkListener", "Spatial", preload("res://wwise/nodes/ak_listener.gd"), preload("res://addons/wwise/wwise_node.png"))
-	add_custom_type("AkState", "Spatial", preload("res://wwise/nodes/ak_state.gd"), preload("res://addons/wwise/wwise_node.png"))
-	add_custom_type("AkSwitch", "Spatial", preload("res://wwise/nodes/ak_switch.gd"), preload("res://addons/wwise/wwise_node.png"))
-	add_custom_type("AkEnvironment", "Area", preload("res://wwise/nodes/ak_environment.gd"), preload("res://addons/wwise/wwise_node.png"))
+	add_custom_type("AkEvent", "Spatial", preload("res://wwise/nodes/ak_event.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
+	add_custom_type("AkBank", "Spatial", preload("res://wwise/nodes/ak_bank.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
+	add_custom_type("AkListener", "Spatial", preload("res://wwise/nodes/ak_listener.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
+	add_custom_type("AkState", "Spatial", preload("res://wwise/nodes/ak_state.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
+	add_custom_type("AkSwitch", "Spatial", preload("res://wwise/nodes/ak_switch.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
+	add_custom_type("AkEnvironment", "Area", preload("res://wwise/nodes/ak_environment.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
 	add_spatial_gizmo_plugin(ak_event_gizmo)
 	
 	# Spatial Audio Nodes
-	add_custom_type("AkGeometry", "Spatial", preload("res://wwise/nodes/ak_geometry.gd"), preload("res://addons/wwise/wwise_node.png"))
-	add_custom_type("AkRoom", "Area", preload("res://wwise/nodes/ak_room.gd"), preload("res://addons/wwise/wwise_node.png"))
-	add_custom_type("AkPortal", "Area", preload("res://wwise/nodes/ak_portal.gd"), preload("res://addons/wwise/wwise_node.png"))
-	add_custom_type("AkEarlyReflections", "Node", preload("res://wwise/nodes/ak_early_reflections.gd"), preload("res://addons/wwise/wwise_node.png"))
+	add_custom_type("AkGeometry", "Spatial", preload("res://wwise/nodes/ak_geometry.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
+	add_custom_type("AkRoom", "Area", preload("res://wwise/nodes/ak_room.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
+	add_custom_type("AkPortal", "Area", preload("res://wwise/nodes/ak_portal.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
+	add_custom_type("AkEarlyReflections", "Node", preload("res://wwise/nodes/ak_early_reflections.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
 	
 	# 2D Nodes
-	add_custom_type("AkEvent2D", "Node2D", preload("res://wwise/nodes/ak_event_2d.gd"), preload("res://addons/wwise/wwise_node.png"))
-	add_custom_type("AkBank2D", "Node2D", preload("res://wwise/nodes/ak_bank_2d.gd"), preload("res://addons/wwise/wwise_node.png"))
-	add_custom_type("AkListener2D", "Node2D", preload("res://wwise/nodes/ak_listener_2d.gd"), preload("res://addons/wwise/wwise_node.png"))
-	add_custom_type("AkState2D", "Node2D", preload("res://wwise/nodes/ak_state_2d.gd"), preload("res://addons/wwise/wwise_node.png"))
-	add_custom_type("AkSwitch2D", "Node2D", preload("res://wwise/nodes/ak_switch_2d.gd"), preload("res://addons/wwise/wwise_node.png"))
+	add_custom_type("AkEvent2D", "Node2D", preload("res://wwise/nodes/ak_event_2d.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
+	add_custom_type("AkBank2D", "Node2D", preload("res://wwise/nodes/ak_bank_2d.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
+	add_custom_type("AkListener2D", "Node2D", preload("res://wwise/nodes/ak_listener_2d.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
+	add_custom_type("AkState2D", "Node2D", preload("res://wwise/nodes/ak_state_2d.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
+	add_custom_type("AkSwitch2D", "Node2D", preload("res://wwise/nodes/ak_switch_2d.gd"), preload("res://addons/wwise_custom_nodes/wwise_node.png"))
 	
 	# Visual Script Nodes
 	VisualScriptEditor.add_custom_node("AkLoadBank", "Wwise", preload("res://wwise/visual nodes/ak_load_bank_vs.gd"))
