8f34f58495849410738b9aac804982d9df3c3939
Jorge Garcia <info@jorgegarciamartin.com>
2020-07-09 18:25:46 +0200

    Added test controls to Wwise GDNative demo main scene. Updated wwise ids

--- wwise-gdnative/gdnative-demo/scripts/3d/testing_stuff.gd
+++ wwise-gdnative/gdnative-demo/scripts/3d/testing_stuff.gd
@@ -4,6 +4,9 @@ export(float) var rtpcValue = 0.0
 var musicVolume:float = -96.0
 var playingID:int = 0
 
+var rtpcSlider = null
+var stateButton = null
+
 export(AK.STATES.MUSICSTATE.STATE._enum) var state = AK.STATES.MUSICSTATE.STATE.CALM
 
 func _ready():
@@ -11,6 +14,23 @@ func _ready():
 	print("Registering GameObject: ", goResult)
 
 	playingID = Wwise.post_event_id(AK.EVENTS.MUSIC, self)
+	
+	rtpcSlider = get_parent().find_node("RTPCSlider")
+	var signalResult = rtpcSlider.connect("value_changed", self, "_onRtpcSliderChanged")
+	assert(signalResult == OK)
+	
+	stateButton = get_parent().find_node("StateButton")
+	signalResult = stateButton.connect("toggled", self, "_onStateButtonToggled")
+	assert(signalResult == OK)
+	
+func _onRtpcSliderChanged(value):
+	Wwise.set_rtpc_id(AK.GAME_PARAMETERS.ENEMIES, value, self)
+	
+func _onStateButtonToggled(button_pressed):
+	if button_pressed:
+		Wwise.set_state_id(AK.STATES.MUSICSTATE.GROUP, AK.STATES.MUSICSTATE.STATE.INTENSE)
+	else:
+		Wwise.set_state_id(AK.STATES.MUSICSTATE.GROUP, AK.STATES.MUSICSTATE.STATE.CALM)
 
 func _process(_delta):
 	musicVolume = Wwise.get_rtpc_id(AK.GAME_PARAMETERS.MUSICVOLUME, self)
@@ -19,9 +39,6 @@ func _process(_delta):
 					   stepify(musicVolume * -0.1, 0.001)))
 					
 	Wwise.set_3d_position(self, get_transform())
-
-	Wwise.set_rtpc_id(AK.GAME_PARAMETERS.ENEMIES, rtpcValue, self)
-	Wwise.set_state_id(AK.STATES.MUSICSTATE.GROUP, state)
 
 func _input(_ev):
 	if Input.is_key_pressed(KEY_P):
