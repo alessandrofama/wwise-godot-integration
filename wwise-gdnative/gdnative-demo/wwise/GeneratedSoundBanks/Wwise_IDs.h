a6f5bc3f68e54e629212a706a73fe414409a1ec7
Alessandro Famà <alessandro.fama@ymail.com>
2020-07-20 19:06:46 +0200

    Convolution test scene

--- wwise-gdnative/gdnative-demo/wwise/GeneratedSoundBanks/Wwise_IDs.h
+++ wwise-gdnative/gdnative-demo/wwise/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,7 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID CONVOLUTION = 3260090173U;
         static const AkUniqueID EXTERNAL_SOURCE_EVENT = 1014047073U;
         static const AkUniqueID FOOTSTEPS = 2385628198U;
         static const AkUniqueID GEOMETRYDEMO = 2626652388U;
@@ -60,6 +61,18 @@ namespace AK
     {
         static const AkUniqueID ENEMIES = 2242381963U;
         static const AkUniqueID MUSICVOLUME = 2346531308U;
+        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
+        static const AkUniqueID RPM = 796049864U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
     } // namespace GAME_PARAMETERS
 
     namespace TRIGGERS
@@ -77,6 +90,7 @@ namespace AK
     {
         static const AkUniqueID GEOMETRYBUS = 4209325213U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MOTION_FACTORY_BUS = 985987111U;
         static const AkUniqueID MUSIC = 3991942870U;
     } // namespace BUSSES
 
@@ -89,6 +103,7 @@ namespace AK
 
     namespace AUDIO_DEVICES
     {
+        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
         static const AkUniqueID NO_OUTPUT = 2317455096U;
         static const AkUniqueID SYSTEM = 3859886410U;
     } // namespace AUDIO_DEVICES
