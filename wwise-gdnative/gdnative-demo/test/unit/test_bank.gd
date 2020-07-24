5c71ea268545886b9d0929564813e77ca4935480
Alessandro <alessandro.fama@ymail.com>
2020-07-24 21:02:27 +0200

    Disabling load_bank string tests since it works only if soundbanks were generated with names [ci skip]

--- wwise-gdnative/gdnative-demo/test/unit/test_bank.gd
+++ wwise-gdnative/gdnative-demo/test/unit/test_bank.gd
@@ -3,13 +3,14 @@ extends "res://addons/gut/test.gd"
 class TestBank:
 	extends "res://addons/gut/test.gd"
 	
-	func test_assert_load_bank_string():
-		# loading banks
-		assert_true(Wwise.load_bank("Init"), "Loading bank should be true")
-		assert_true(Wwise.load_bank("TestBank"), "Loading bank should be true")
+	# Only works if soundbanks generated with names
+	# func test_assert_load_bank_string():
+	# 	# loading banks
+	# 	assert_true(Wwise.load_bank("Init"), "Loading bank should be true")
+	# 	assert_true(Wwise.load_bank("TestBank"), "Loading bank should be true")
 		
-		Wwise.unload_bank("TestBank")
-		Wwise.unload_bank("Init")
+	# 	Wwise.unload_bank("TestBank")
+	# 	Wwise.unload_bank("Init")
 
 		
 	func test_assert_load_bank_id():
@@ -19,13 +20,13 @@ class TestBank:
 		Wwise.unload_bank_id(AK.BANKS.TESTBANK)
 		Wwise.unload_bank_id(AK.BANKS.INIT)
 
-	
-	func test_assert_unload_bank_string():
-		Wwise.load_bank("Init")
-		Wwise.load_bank("TestBank")
+	# Only works if soundbanks generated with names
+	# func test_assert_unload_bank_string():
+	# 	Wwise.load_bank("Init")
+	# 	Wwise.load_bank("TestBank")
 		
-		assert_true(Wwise.unload_bank("TestBank"), "Unloading bank should be true")		
-		assert_true(Wwise.unload_bank("Init"), "Unloading bank should be true")
+	# 	assert_true(Wwise.unload_bank("TestBank"), "Unloading bank should be true")		
+	# 	assert_true(Wwise.unload_bank("Init"), "Unloading bank should be true")
 	
 	func test_assert_unload_bank_id():
 		Wwise.load_bank_id(AK.BANKS.INIT)
