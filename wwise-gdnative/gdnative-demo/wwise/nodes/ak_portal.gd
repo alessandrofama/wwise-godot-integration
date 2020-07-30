79b9c38345d91682f1738c653df595f1c599d80f
Alessandro <alessandro.fama@ymail.com>
2020-07-30 15:59:44 +0200

    Added ability to update portal with set_portal function when setting the enabled variable [ci skip]

--- wwise-gdnative/gdnative-demo/wwise/nodes/ak_portal.gd
+++ wwise-gdnative/gdnative-demo/wwise/nodes/ak_portal.gd
@@ -5,7 +5,7 @@ export(NodePath) var front_room:NodePath
 var front_room_node
 export(NodePath) var back_room:NodePath
 var back_room_node
-export(bool) var enabled:bool = true
+export(bool) var enabled:bool = true setget set_enabled
 
 func _ready():
 	if not front_room.is_empty():		
@@ -13,5 +13,13 @@ func _ready():
 	if not back_room.is_empty():
 		back_room_node = get_node(back_room)
 	extent = self.get_scale()
+	set_portal()
+
+func set_enabled(value: bool) -> void:
+	if enabled != value:
+		enabled = value
+		set_portal()
+
+func set_portal() -> void:
 	Wwise.set_portal(self, self.get_global_transform(), extent, front_room_node, 
-					back_room_node, enabled, self.get_name())
+	back_room_node, enabled, self.get_name())
