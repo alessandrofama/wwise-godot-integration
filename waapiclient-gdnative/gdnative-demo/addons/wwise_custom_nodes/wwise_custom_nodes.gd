2be38e93e2a75d41357819ef1fbc411ef0f73a6c
Alessandro <me@alessandrofama.com>
2020-09-27 13:55:01 +0200

    Setting the reloadable property of the WAAPI libs to false, fixes issue #1

--- waapiclient-gdnative/gdnative-demo/addons/wwise_custom_nodes/wwise_custom_nodes.gd
+++ waapiclient-gdnative/gdnative-demo/addons/wwise_custom_nodes/wwise_custom_nodes.gd
@@ -7,6 +7,7 @@ var ak_event_gizmo = AkEventGizmo.new()
 
 func _enter_tree():
 	add_custom_type("AkEvent", "Spatial", preload("res://wwise/runtime/nodes/ak_event.gd"), ResourceLoader.load("res://addons/wwise_custom_nodes/wwise_node.png"))
+	add_spatial_gizmo_plugin(ak_event_gizmo)
 			
 func _exit_tree():
 	remove_custom_type("AkEvent")
