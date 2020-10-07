e07e00b48e04c9161177bc836cc4c36841c27dd2
Alessandro <me@alessandrofama.com>
2020-10-07 21:59:27 +0200

    Disabling Mac menu on integration app

--- integration-app/src/background.js
+++ integration-app/src/background.js
@@ -1,6 +1,6 @@
 "use strict";
 import path from "path";
-import { app, protocol, BrowserWindow } from "electron";
+import { app, protocol, BrowserWindow, Menu } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
 const isDevelopment = process.env.NODE_ENV !== "production";
@@ -44,6 +44,7 @@ function createWindow() {
     // Load the index.html when not in development
     win.loadURL("app://./index.html");
     win.setMenu(null);
+    Menu.setApplicationMenu(false);
   }
 
   win.on("closed", () => {
