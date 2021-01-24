4accabbe6ce739c7417f4c2a176c9434a0a64bdf
Alessandro Famà <me@alessandrofama.com>
2021-01-24 23:09:38 +0100

    Regenerated macOS and Windows banks for demo project

--- wwise-gdnative/gdnative-demo/wwise/GeneratedSoundBanks/Wwise_IDs.h
+++ wwise-gdnative/gdnative-demo/wwise/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,20 +13,13 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID CONVOLUTION = 3260090173U;
         static const AkUniqueID EXTERNAL_SOURCE_EVENT = 1014047073U;
         static const AkUniqueID FOOTSTEPS = 2385628198U;
         static const AkUniqueID GEOMETRYDEMO = 2626652388U;
-        static const AkUniqueID MOTION_AUX = 2297584640U;
-        static const AkUniqueID MOTION_SOURCE = 2209794929U;
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID MUSIC_2D = 1939884427U;
         static const AkUniqueID MUSIC_LONG_ATT = 1272633243U;
         static const AkUniqueID PLAY_CHIMES_WITH_MARKER = 3900723121U;
-        static const AkUniqueID REFLECT = 243379636U;
-        static const AkUniqueID SOUNDSEED_GRAIN = 3404211905U;
-        static const AkUniqueID SOUNDSEED_IMPACT = 480515238U;
-        static const AkUniqueID SOUNDSEED_WIND = 623594218U;
         static const AkUniqueID SYNTH_ONE_TEST = 2343727715U;
     } // namespace EVENTS
 
@@ -67,18 +60,6 @@ namespace AK
     {
         static const AkUniqueID ENEMIES = 2242381963U;
         static const AkUniqueID MUSICVOLUME = 2346531308U;
-        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
-        static const AkUniqueID RPM = 796049864U;
-        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
-        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
-        static const AkUniqueID SS_AIR_FURY = 1029930033U;
-        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
-        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
-        static const AkUniqueID SS_AIR_RPM = 822163944U;
-        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
-        static const AkUniqueID SS_AIR_STORM = 3715662592U;
-        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
-        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
     } // namespace GAME_PARAMETERS
 
     namespace TRIGGERS
@@ -89,7 +70,6 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID PLUGINS = 2651853335U;
         static const AkUniqueID TESTBANK = 3291379323U;
     } // namespace BANKS
 
@@ -97,23 +77,18 @@ namespace AK
     {
         static const AkUniqueID GEOMETRYBUS = 4209325213U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
-        static const AkUniqueID MASTER_PLUGINS = 431572087U;
-        static const AkUniqueID MOTION_FACTORY_BUS = 985987111U;
         static const AkUniqueID MUSIC = 3991942870U;
     } // namespace BUSSES
 
     namespace AUX_BUSSES
     {
         static const AkUniqueID LARGEVERB = 2757439665U;
-        static const AkUniqueID MOTION_AUX = 2297584640U;
-        static const AkUniqueID REFLECTAUX = 1692308750U;
         static const AkUniqueID ROOMVERB = 1572913279U;
         static const AkUniqueID SMALLVERB = 117523933U;
     } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
-        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
         static const AkUniqueID NO_OUTPUT = 2317455096U;
         static const AkUniqueID SYSTEM = 3859886410U;
     } // namespace AUDIO_DEVICES
