d3d6a1279b25f2dd01422e8b8d799c19ec1cf7fe
Alessandro Famà <me@alessandrofama.com>
2022-04-27 21:16:41 +0200

    Fix windows wwise-gdnative build

--- wwise-gdnative/src/wwise_gdnative.h
+++ wwise-gdnative/src/wwise_gdnative.h
@@ -32,9 +32,7 @@
 #include <AK/Comm/AkCommunication.h>
 #endif
 
-#if defined(AK_LINUX) || defined(AK_MAC_OS_X) || defined(AK_ANDROID) || defined(AK_IOS)
 #include <memory>
-#endif
 
 namespace godot
 {
