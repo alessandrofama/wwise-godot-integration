2e7eb770ffdc32acb59bc8cae1e57696bbb36711
Alessandro Famà <me@alessandrofama.com>
2024-11-21 14:06:23 +0100

    Load Init Bank at startup by default (#125)

--- addons/Wwise/native/src/core/wwise_settings.cpp
+++ addons/Wwise/native/src/core/wwise_settings.cpp
@@ -26,7 +26,7 @@ void WwiseSettings::add_wwise_settings()
 	add_setting(WWISE_COMMON_USER_SETTINGS_PATH + "base_path", String("res://GeneratedSoundBanks"),
 			Variant::Type::STRING, PROPERTY_HINT_DIR, "res://GeneratedSoundBanks");
 	add_setting(WWISE_COMMON_USER_SETTINGS_PATH + "startup_language", String("English(US)"), Variant::Type::STRING);
-	add_setting(WWISE_COMMON_USER_SETTINGS_PATH + "load_init_bank_at_startup", false, Variant::Type::BOOL);
+	add_setting(WWISE_COMMON_USER_SETTINGS_PATH + "load_init_bank_at_startup", true, Variant::Type::BOOL);
 	add_setting(WWISE_COMMON_USER_SETTINGS_PATH + "suspend_on_focus_loss", false, Variant::Type::BOOL);
 	add_setting(WWISE_COMMON_USER_SETTINGS_PATH + "use_soundbank_names", true, Variant::Type::BOOL);
 	add_setting(WWISE_COMMON_USER_SETTINGS_PATH + "engine_logging", false, Variant::Type::BOOL);
