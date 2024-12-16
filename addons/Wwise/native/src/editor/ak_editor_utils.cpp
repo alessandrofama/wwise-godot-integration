15a3a81bf1c80c73274bbf5b46fda7d3c8ab72f7
Alessandro Famà <me@alessandrofama.com>
2024-12-16 21:09:55 +0100

    Refactor AkEditorUtils for Editor Icons
    Refactored AkEditorUtils to optimize and simplify the process of retrieving editor icons for all Wwise objects in the integration. Using a simple cache reduced the overhead caused by repeated use of ResourceLoader. The cache ensures that icons are only loaded once per session, addressing the performance bottleneck associated with slow resource loading (ResourceLoader is slow when trying to load Icons for hundreds of Objects in the Picker or Browser for example).

--- addons/Wwise/native/src/editor/ak_editor_utils.cpp
+++ addons/Wwise/native/src/editor/ak_editor_utils.cpp
@@ -1,45 +1,71 @@
 #include "ak_editor_utils.h"
 
-AkEditorUtils* AkEditorUtils::singleton = nullptr;
+Ref<Texture2D> AkEditorUtils::icon_cache[2][(int)WwiseObjectType::Max] = {};
 
-AkEditorUtils::AkEditorUtils()
-{
-	ERR_FAIL_COND(singleton != nullptr);
-	singleton = this;
-}
-
-AkEditorUtils::~AkEditorUtils()
-{
-	ERR_FAIL_COND(singleton != this);
-	singleton = nullptr;
-}
-
-AkEditorUtils* AkEditorUtils::get_singleton() { return singleton; }
-
-Ref<Texture2D> AkEditorUtils::get_editor_icon(const AkType ak_type)
-{
-	const AkEditorIconType icon_type = get_ak_editor_icon_type(ak_type);
-	return get_editor_icon(icon_type);
-}
-
-Ref<Texture2D> AkEditorUtils::get_editor_icon(const AkEditorIconType icon_type)
+Ref<Texture2D> AkEditorUtils::get_editor_icon(const WwiseObjectType p_type)
 {
 	ERR_FAIL_COND_V_MSG(
-			icon_type >= AK_ICON_LENGTH, Ref<Texture2D>(), "[Wwise Editor] Tried to get invalid editor icon.");
+			p_type >= WwiseObjectType::Max, Ref<Texture2D>(), "WwiseGodot: Tried to get invalid editor icon.");
 
-	Ref<Texture2D> icon = cached_icons[icon_type];
-	if (icon.is_valid())
+	bool dark_mode = DisplayServer::get_singleton()->is_dark_mode();
+	int mode_index = dark_mode ? 1 : 0;
+
+	if (icon_cache[mode_index][(int)p_type].is_valid())
 	{
-		return icon;
+		return icon_cache[mode_index][(int)p_type];
 	}
 
-	const String icon_path = get_ak_editor_icon_path(icon_type);
-	icon = ResourceLoader::get_singleton()->load(icon_path);
+	String icon_path = get_theme_folder(dark_mode) + get_icon_name(p_type);
+	Ref<Texture2D> icon = ResourceLoader::get_singleton()->load(icon_path);
 
 	if (icon.is_valid())
 	{
-		cached_icons[icon_type] = icon;
+		icon_cache[mode_index][(int)p_type] = icon;
 	}
 
 	return icon;
+}
+
+String AkEditorUtils::get_icon_name(const WwiseObjectType p_type)
+{
+	switch (p_type)
+	{
+		case WwiseObjectType::AuxBus:
+			return "ObjectIcons_AuxBus_nor.svg";
+		case WwiseObjectType::Bus:
+			return "ObjectIcons_Bus_nor.svg";
+		case WwiseObjectType::Event:
+			return "ObjectIcons_Event_nor.svg";
+		case WwiseObjectType::Folder:
+			return "ObjectIcons_Folder_nor.svg";
+		case WwiseObjectType::PhysicalFolder:
+			return "ObjectIcons_PhysicalFolder_nor.svg";
+		case WwiseObjectType::Project:
+			return "ObjectIcons_Project_nor.svg";
+		case WwiseObjectType::Soundbank:
+			return "ObjectIcons_Soundbank_nor.svg";
+		case WwiseObjectType::State:
+			return "ObjectIcons_State_nor.svg";
+		case WwiseObjectType::StateGroup:
+			return "ObjectIcons_StateGroup_nor.svg";
+		case WwiseObjectType::Switch:
+			return "ObjectIcons_Switch_nor.svg";
+		case WwiseObjectType::SwitchGroup:
+			return "ObjectIcons_SwitchGroup_nor.svg";
+		case WwiseObjectType::Workunit:
+			return "ObjectIcons_Workunit_nor.svg";
+		case WwiseObjectType::GameParameter:
+			return "ObjectIcons_GameParameter_nor.svg";
+		case WwiseObjectType::Trigger:
+			return "ObjectIcons_Trigger_nor.svg";
+		case WwiseObjectType::AcousticTexture:
+			return "ObjectIcons_AcousticTexture_nor.svg";
+		default:
+			return "ObjectIcons_Project_nor.svg";
+	}
+}
+
+String AkEditorUtils::get_theme_folder(bool dark_mode)
+{
+	return dark_mode ? "res://addons/Wwise/editor/theme/dark/images/" : "res://addons/Wwise/editor/theme/light/images/";
 }
