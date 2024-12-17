d5d8d34e82dc7ebdef83f9e27a2f4a6b4ace231f
Alessandro Famà <me@alessandrofama.com>
2024-12-17 20:54:39 +0100

    Update class registration routine for new Wwise types
    Updated the class registration routine in wwise_gdextension_main.cpp to include all newly added classes in the integration. This ensures that the new Wwise Types, editor plugins, and supporting utilities are correctly registered and available within the engine.
    register platform info plugin info

--- addons/Wwise/native/src/wwise_gdextension_main.cpp
+++ addons/Wwise/native/src/wwise_gdextension_main.cpp
@@ -1,16 +1,36 @@
-#include <algorithm>
-#include <memory>
+#if defined(TOOLS_ENABLED)
+#include "editor/plugins/ak_json_builder.cpp"
+#include "editor/plugins/ak_soundbank_directory_watcher_plugin.cpp"
+#include "editor/wwise_project_database.cpp"
+#endif
 
+#include "core/ak_midi_post.h"
 #include "core/ak_utils.cpp"
-#include "core/wwise_cookie.cpp"
+#include "core/types/wwise_acoustic_texture.h"
+#include "core/types/wwise_aux_bus.cpp"
+#include "core/types/wwise_bank.cpp"
+#include "core/types/wwise_base_type.cpp"
+#include "core/types/wwise_event.cpp"
+#include "core/types/wwise_group_type.cpp"
+#include "core/types/wwise_rtpc.cpp"
+#include "core/types/wwise_state.cpp"
+#include "core/types/wwise_state_group.h"
+#include "core/types/wwise_switch.cpp"
+#include "core/types/wwise_switch_group.h"
+#include "core/types/wwise_trigger.cpp"
 #include "core/wwise_gdextension.cpp"
 #include "core/wwise_io_hook.cpp"
+#include "core/wwise_platform_info.cpp"
+#include "core/wwise_plugin_info.cpp"
 #include "core/wwise_settings.cpp"
 #include "scene/ak_bank.cpp"
 #include "scene/ak_early_reflections.cpp"
 #include "scene/ak_environment.cpp"
 #include "scene/ak_environment_data.cpp"
 #include "scene/ak_event.cpp"
+#include "scene/ak_game_obj.h"
+#include "scene/ak_game_obj_2d.h"
+#include "scene/ak_game_obj_3d.h"
 #include "scene/ak_geometry.cpp"
 #include "scene/ak_listener.cpp"
 #include "scene/ak_portal.cpp"
@@ -19,29 +39,45 @@
 #include "scene/ak_switch.cpp"
 
 #if defined(TOOLS_ENABLED)
-#include "gen/doc_data.gen.cpp"
-#include "editor/ak_android_export_plugin.cpp"
-#include "editor/ak_editor_export_plugin.cpp"
-#include "editor/ak_editor_plugin.cpp"
+#include "editor/ak_editor_settings.h"
 #include "editor/ak_editor_utils.cpp"
-#include "editor/ak_event_3d_gizmo_plugin.cpp"
-#include "editor/ak_inspector_plugin.cpp"
+#include "editor/ak_wwise_inspector_picker.cpp"
+#include "editor/ak_wwise_tree.cpp"
+#include "editor/ak_wwise_tree_data_source.cpp"
+#include "editor/ak_wwise_tree_item.cpp"
+#include "editor/plugins/ak_android_export_plugin.cpp"
+#include "editor/plugins/ak_editor_export_plugin.cpp"
+#include "editor/plugins/ak_editor_plugin.cpp"
+#include "editor/plugins/ak_event_3d_gizmo_plugin.cpp"
+#include "editor/plugins/ak_wwise_editor_inspector_plugin.cpp"
+#include "editor/plugins/wwise_browser.cpp"
+#include "editor/plugins/wwise_project_info.cpp"
+#include "editor/properties/ak_wwise_editor_property.cpp"
+#include "editor/properties/ak_wwise_editor_property_acoustic_texture.cpp"
+#include "editor/properties/ak_wwise_editor_property_aux_bus.cpp"
+#include "editor/properties/ak_wwise_editor_property_bank.cpp"
+#include "editor/properties/ak_wwise_editor_property_event.cpp"
+#include "editor/properties/ak_wwise_editor_property_rtpc.cpp"
+#include "editor/properties/ak_wwise_editor_property_state.cpp"
+#include "editor/properties/ak_wwise_editor_property_switch.cpp"
+#include "editor/properties/ak_wwise_editor_property_trigger.cpp"
+#include "editor/wwise_base_information.cpp"
+#include "editor/wwise_project_data.cpp"
+#include "editor/wwise_tree_object.cpp"
+#include "editor/wwise_tree_object_event.cpp"
+#include "gen/doc_data.gen.cpp"
 #endif
 
 #if defined(AK_WIN) || defined(AK_MAC_OS_X)
 #include "core/waapi_gdextension.cpp"
-#if defined(TOOLS_ENABLED)
-#include "editor/ak_waapi_picker.cpp"
-#endif
 static Waapi* waapi_module;
 #endif
 
 static Wwise* wwise_module;
 static WwiseSettings* wwise_settings;
 static AkUtils* ak_utils;
-
 #if defined(TOOLS_ENABLED)
-static AkEditorUtils* ak_editor_utils;
+static WwiseProjectDatabase* wwise_project_database;
 #endif
 
 void register_wwise_types(ModuleInitializationLevel p_level)
@@ -52,26 +88,47 @@ void register_wwise_types(ModuleInitializationLevel p_level)
 		ClassDB::register_class<Waapi>();
 		waapi_module = memnew(Waapi);
 		Engine::get_singleton()->register_singleton("Waapi", Waapi::get_singleton());
-#if defined(TOOLS_ENABLED)
-		ClassDB::register_class<AkWaapiPicker>();
-		EditorPlugins::add_by_type<AkWaapiPicker>();
-#endif
 #endif
 
 #if defined(AK_WIN) || defined(AK_MAC_OS_X) || defined(AK_LINUX)
 #if defined(TOOLS_ENABLED)
-		ClassDB::register_class<AkEditorUtils>();
-		ak_editor_utils = memnew(AkEditorUtils);
-		Engine::get_singleton()->register_singleton("AkEditorUtils", AkEditorUtils::get_singleton());
-		ClassDB::register_class<AkInspectorEditor>();
-		ClassDB::register_class<AkInspectorTree>();
-		ClassDB::register_class<AkInspectorEditorInspectorPlugin>();
-		ClassDB::register_class<AkInspectorEditorProperty>();
+		ClassDB::register_abstract_class<AkWwiseEditorProperty>();
+		ClassDB::register_class<AkWwiseEditorInspectorPlugin>();
+		ClassDB::register_class<AkWwiseEditorPropertyEvent>();
+		ClassDB::register_class<AkWwiseEditorPropertyBank>();
+		ClassDB::register_class<AkWwiseEditorPropertyAcousticTexture>();
+		ClassDB::register_class<AkWwiseEditorPropertyState>();
+		ClassDB::register_class<AkWwiseEditorPropertySwitch>();
+		ClassDB::register_class<AkWwiseEditorPropertyRTPC>();
+		ClassDB::register_class<AkWwiseEditorPropertyTrigger>();
+		ClassDB::register_class<AkWwiseEditorPropertyAuxBus>();
+
+		ClassDB::register_class<AkWwiseTreeItem>();
+		ClassDB::register_class<AkWwiseTree>();
+		ClassDB::register_class<AkWwiseTreeDataSource>();
+		ClassDB::register_class<AkWwiseInspectorPicker>();
+
 		ClassDB::register_class<AkEvent3DGizmoPlugin>();
 		ClassDB::register_class<AkEditorExportPlugin>();
 		ClassDB::register_class<AkAndroidExportPlugin>();
+		ClassDB::register_class<WwiseBaseInformation>();
+		ClassDB::register_class<WwiseTreeObject>();
+		ClassDB::register_class<WwiseTreeObjectEvent>();
+		ClassDB::register_class<WwiseProjectData>();
+		ClassDB::register_class<WwiseProjectDatabase>();
+		wwise_project_database = memnew(WwiseProjectDatabase);
+		Engine::get_singleton()->register_singleton("WwiseProjectDatabase", WwiseProjectDatabase::get_singleton());
+		ClassDB::register_class<WwiseProjectInfo>();
+		EditorPlugins::add_by_type<WwiseProjectInfo>();
+		ClassDB::register_class<AkSoundBankDirectoryWatcherPlugin>();
+		EditorPlugins::add_by_type<AkSoundBankDirectoryWatcherPlugin>();
+		ClassDB::register_class<AkJSONBuilder>();
+		EditorPlugins::add_by_type<AkJSONBuilder>();
 		ClassDB::register_class<AkEditorPlugin>();
 		EditorPlugins::add_by_type<AkEditorPlugin>();
+		ClassDB::register_class<WwiseBrowser>();
+		EditorPlugins::add_by_type<WwiseBrowser>();
+		ClassDB::register_class<AkEditorUtils>();
 #endif
 #endif
 	}
@@ -89,7 +146,25 @@ void register_wwise_types(ModuleInitializationLevel p_level)
 		ClassDB::register_class<WwiseSettings>();
 		wwise_settings = memnew(WwiseSettings);
 
-		ClassDB::register_class<WwiseCookie>();
+		ClassDB::register_abstract_class<WwiseBaseType>();
+		ClassDB::register_abstract_class<WwiseGroupType>();
+		ClassDB::register_class<WwiseEvent>();
+		ClassDB::register_class<WwiseBank>();
+		ClassDB::register_class<WwiseAcousticTexture>();
+		ClassDB::register_class<WwiseState>();
+		ClassDB::register_class<WwiseStateGroup>();
+		ClassDB::register_class<WwiseSwitch>();
+		ClassDB::register_class<WwiseSwitchGroup>();
+		ClassDB::register_class<WwiseRTPC>();
+		ClassDB::register_class<WwiseTrigger>();
+		ClassDB::register_class<WwiseAuxBus>();
+		ClassDB::register_class<AkMidiPost>();
+		ClassDB::register_class<WwisePluginInfo>();
+		ClassDB::register_class<WwisePlatformInfo>();
+
+		ClassDB::register_class<AkGameObj>();
+		ClassDB::register_class<AkGameObj2D>();
+		ClassDB::register_class<AkGameObj3D>();
 		ClassDB::register_class<AkBank>();
 		ClassDB::register_class<AkListener2D>();
 		ClassDB::register_class<AkListener3D>();
@@ -136,10 +211,10 @@ void unregister_wwise_types(ModuleInitializationLevel p_level)
 
 #if defined(AK_WIN) || defined(AK_MAC_OS_X) || defined(AK_LINUX)
 #if defined(TOOLS_ENABLED)
-		if (Engine::get_singleton()->has_singleton("AkEditorUtils"))
+		if (Engine::get_singleton()->has_singleton("WwiseProjectDatabase"))
 		{
-			Engine::get_singleton()->unregister_singleton("AkEditorUtils");
-			memdelete(ak_editor_utils);
+			Engine::get_singleton()->unregister_singleton("WwiseProjectDatabase");
+			memdelete(wwise_project_database);
 		}
 #endif
 #endif
@@ -148,8 +223,6 @@ void unregister_wwise_types(ModuleInitializationLevel p_level)
 
 extern "C"
 {
-	// Initialization.
-
 	GDExtensionBool GDE_EXPORT wwise_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address,
 			GDExtensionClassLibraryPtr p_library, GDExtensionInitialization* r_initialization)
 	{
@@ -158,7 +231,6 @@ extern "C"
 		init_obj.register_initializer(register_wwise_types);
 		init_obj.register_terminator(unregister_wwise_types);
 		init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SERVERS);
-
 		return init_obj.init();
 	}
 }
