348161a1ef8d9dcab423d0346d613f3642a52998
Alessandro Famà <me@alessandrofama.com>
2026-09-24 16:34:16 +0200

    Update integration to Wwise 2024.1.17

--- addons/Wwise/native/src/editor/ak_editor_settings.h
+++ addons/Wwise/native/src/editor/ak_editor_settings.h
@@ -172,7 +172,8 @@ enum class PluginID : uint32_t
 	McDSPLimiter = 0x671003,
 	ResonanceAudioRoomEffect = 0xC81106,
 	IgniterLive = 0x5110D2,
-	IgniterLiveSynth = 0x5210D2
+	IgniterLiveSynth = 0x5210D2,
+	Tempest3dAudioFX = 0x00D30003
 };
 
 static const std::unordered_set<PluginID> always_skip_plugin_ids = { PluginID::SineGenerator, PluginID::SinkAuxiliary,
@@ -202,7 +203,10 @@ static const std::unordered_map<PluginID, const char*> ios_header_mapping = { { 
 	{ PluginID::iZTrashDistortion, "iZTrashDistortionFXFactory" },
 	{ PluginID::iZTrashDynamics, "iZTrashDynamicsFXFactory" }, { PluginID::iZTrashFilters, "iZTrashFiltersFXFactory" },
 	{ PluginID::iZTrashMultibandDistortion, "iZTrashMultibandDistortionFXFactory" },
-	{ PluginID::McDSPFutzBox, "McDSPFutzBoxFXFactory" }, { PluginID::McDSPLimiter, "McDSPLimiterFXFactory" },
-	{ PluginID::ResonanceAudioRoomEffect, "ResonanceAudioFXFactory" } };
+	{ PluginID::McDSPFutzBox, "McDSPFutzBoxFXFactory" },
+	{ PluginID::McDSPLimiter, "McDSPLimiterFXFactory" },
+	{ PluginID::ResonanceAudioRoomEffect, "ResonanceAudioFXFactory" }, 
+	{ PluginID::Tempest3dAudioFX, "Tempest3dAudioFXFactory" } 
+};
 
 } //namespace AkEditorSettings
